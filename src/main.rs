851db6b15cea685e24a802884f6d8219e7d67480
Oliver Anhuth <oliver.anhuth@web.de>
2020-06-25 11:10:56 +0200

    Make test more robust.

--- src/main.rs
+++ src/main.rs
@@ -38,26 +38,35 @@ mod tests {
 
     #[test]
     fn test_pbnorm() {
-        for file in [
+        let test_files = [
             "PB_Kontoauszug_KtoNr_1234567890_06-01-2020_043030.pdf",
             "PB_Kontoauszug_KtoNr_1111111111_06-02-2020_043030.pdf",
             "PB_Kontoauszug_KtoNr_2222222222_06-03-2020_043030.pdf",
             "PB_Kontoauszug_KtoNr_3333333333_06-04-2020_043030.pdf",
             "PB_Kontoauszug_KtoNr_4444444444_06-05-2020_043030.pdf",
-        ].iter() {
-            File::create(file).expect("Could not create test file");
-        }
-        pbnorm().expect("Errors during test");
-        for result_file in [
+        ];
+
+        let expected_result_files = [
             "Postbank-2020-01-06.pdf",
             "Postbank-2020-02-06.pdf",
             "Postbank-2020-03-06.pdf",
             "Postbank-2020-04-06.pdf",
             "Postbank-2020-05-06.pdf",
-        ].iter() {
-            let result_file_path = Path::new(&result_file);
-            assert!(result_file_path.is_file(), "Expected result file {} not found", result_file);
-            fs::remove_file(result_file_path).ok();
+        ];
+
+        for result_file in expected_result_files.iter() {
+            fs::remove_file(result_file).ok();
+        }
+
+        for file in test_files.iter() {
+            File::create(file).expect("Could not create test file");
+        }
+
+        pbnorm().expect("Errors during test");
+
+        for result_file in expected_result_files.iter() {
+            assert!(Path::new(result_file).is_file(), "Expected result file {} not found", result_file);
+            fs::remove_file(result_file).ok();
         }
     }
 }
