4a6e3547c8faf76bd654ea27b623e8f41c82771c
Oliver Anhuth <oliver.anhuth@web.de>
2021-04-15 09:58:06 +0200

    Apply auto formatter to TypeScript files.

--- pbnorm.ts
+++ pbnorm.ts
@@ -3,18 +3,18 @@
 const re = /^PB_Kontoauszug_KtoNr_\d{10}_(\d{2})-(\d{2})-(\d{4})_\d{6}.pdf$/;
 
 export function pbnorm() {
-    for (const entry of Deno.readDirSync(".")) {
-        if (entry.isFile) {
-            const match = entry.name.match(re);
-            if (match) {
-                const [, day, month, year] = match;
-                const to = `Postbank-${year}-${month}-${day}.pdf`
-                Deno.renameSync(entry.name, to);
-            }
-        }
+  for (const entry of Deno.readDirSync(".")) {
+    if (entry.isFile) {
+      const match = entry.name.match(re);
+      if (match) {
+        const [, day, month, year] = match;
+        const to = `Postbank-${year}-${month}-${day}.pdf`;
+        Deno.renameSync(entry.name, to);
+      }
     }
+  }
 }
 
 if (import.meta.main) {
-    pbnorm();
+  pbnorm();
 }
